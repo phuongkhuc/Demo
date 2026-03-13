782911299cab50f945bfaa2ca8e1c5f8e0da4261
phuongkhuc <phuongkhuc000@gmail.com>
2026-03-13 23:53:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,6 @@ def load_model():
 
 st.sidebar.header("Customer Information")
 national_id = st.sidebar.text_input("National ID")
-customer_type = detect_customer_type(national_id, internal_df)
 
 dob = st.sidebar.date_input(
     "Date of Birth"
@@ -342,7 +341,7 @@ def decision_matrix(customer_type,risk,credit_score,dti_2,
         return "Reject",0
 
     # ---------- ETB ----------
-
+    customer_type = detect_customer_type(national_id, internal_df)
                         
     if customer_type == "ETB":
 
