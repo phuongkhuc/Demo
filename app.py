0eee872eab58c2cecef60dc85c699a52ac48b3ad
phuongkhuc <phuongkhuc000@gmail.com>
2026-03-14 15:38:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -530,7 +530,7 @@ if st.sidebar.button("Evaluate Application"):
     col1,col2,col3 = st.columns(3)
 
     with col1:
-       with st.container():
+        with st.container():
            st.markdown('<div class="kpi">', unsafe_allow_html=True)
            st.metric(
                "Approval Probability",
@@ -539,23 +539,23 @@ if st.sidebar.button("Evaluate Application"):
     )
 
     with col2:
-          if risk < 0.5:
+        if risk < 0.5:
                level = "🔴 High Default Risk"
-          elif risk < 0.7:
+        elif risk < 0.7:
                level = "🟡 Medium Risk"
-          else:
+        else:
                level = "🟢 Low Default Risk"
             
-       with st.container():   
-          st.markdown('<div class="kpi">', unsafe_allow_html=True)
-          st.metric("Risk Level",level)
-          st.markdown('</div>', unsafe_allow_html=True)
+        with st.container():   
+            st.markdown('<div class="kpi">', unsafe_allow_html=True)
+            st.metric("Risk Level",level)
+            st.markdown('</div>', unsafe_allow_html=True)
 
     with col3:
-       with st.container():  
-          st.markdown('<div class="kpi">', unsafe_allow_html=True)
-          st.metric("Rule Engine",rule_result)
-          st.markdown('</div>', unsafe_allow_html=True)
+        with st.container():  
+            st.markdown('<div class="kpi">', unsafe_allow_html=True)
+            st.metric("Rule Engine",rule_result)
+            st.markdown('</div>', unsafe_allow_html=True)
 
     
     #Financial Capacity Card
